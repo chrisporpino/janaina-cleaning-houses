c3f5710f03ce79f6fc3013a867652014a4268211
agenciaaudax <agenciaaudax@gmail.com>
2021-01-06 16:19:13 -0300

    essas saos as atualizacoes relacionadas as const

--- src/containers/ServicesSection/index.js
+++ src/containers/ServicesSection/index.js
@@ -1,77 +1,128 @@
 import React from "react";
-import "../../assets/styles/global.css";
+// import "../../assets/styles/global.css";
 import styled from "styled-components";
-import "../../App.css";
+// import "../../App.css";
+
+const Services = styled.div`
+  width: 100%;
+  background-color: white;
+`;
+
+const ServicesTitle = styled.h2`
+  text-align: center;
+  height: 28px;
+  padding: 81px 673px 54px 673px;
+`;
+
+const ServGeral = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+const ServiceBox = styled.div`
+  width: 351px;
+  height: 263px;
+  background-color: #3ab3b2;
+`;
+
+const ServiceBoxImage = styled.img`
+  width: 314px;
+  height: 129px;
+  margin: 14px 18px 7px 19px;
+`;
+
+const ServiceBoxTitle = styled.h4`
+  width: 155px;
+  height: 19px;
+  margin: 0px 178px 12px 18px;
+`;
+
+const ServiceBoxParagraph = styled.p`
+  width: 314px;
+  height: 32px;
+  margin: 0px 19px 50px 18px;
+`;
+
+const ServButton = styled.button`
+  display: inline-block;
+  position: absolute;
+  width: 204px;
+  height: 55px;
+  top: 1890px;
+  left: 650px;
+  border-radius: 5px;
+`;
 
 function ServicesSection() {
   return (
-    <div id="services">
-      <h2 id="services-title">SERVICES</h2>
+    <Services>
+      <ServicesTitle>SERVICES</ServicesTitle>
 
-      <div id="serv-geral">
-        <div id="Service-box" class="services-1">
-          <img
+      <ServGeral>
+        <ServiceBox>
+          <ServiceBoxImage
             src="https://images.unsplash.com/photo-1577412647305-991150c7d163?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fG9mZmljZXxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <h4>Office cleaning</h4>
-          <p>
+          <ServiceBoxTitle>Office cleaning</ServiceBoxTitle>
+          <ServiceBoxParagraph>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
             accusantium debitis doloribus, repellendus quas ut.
-          </p>
-        </div>
+          </ServiceBoxParagraph>
+        </ServiceBox>
 
-        <div id="Service-box" class="services-2">
-          <img
+        <ServiceBox>
+          <ServiceBoxImage
             src="https://images.unsplash.com/photo-1516455590571-18256e5bb9ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGhvbWV8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <h4>Home cleaning</h4>
-          <p>
+          <ServiceBoxTitle>Home cleaning</ServiceBoxTitle>
+          <ServiceBoxParagraph>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
             accusantium debitis doloribus, repellendus quas ut.
-          </p>
-        </div>
+          </ServiceBoxParagraph>
+        </ServiceBox>
 
-        <div id="Service-box" class="services-3">
-          <img
+        <ServiceBox>
+          <ServiceBoxImage
             src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGhvbWV8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <h4>Flat cleaning</h4>
-          <p>
+          <ServiceBoxTitle>Flat cleaning</ServiceBoxTitle>
+          <ServiceBoxParagraph>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
             accusantium debitis doloribus, repellendus quas ut.
-          </p>
-        </div>
+          </ServiceBoxParagraph>
+        </ServiceBox>
 
-        <div id="Service-box" class="services-4">
-          <img
+        <ServiceBox>
+          <ServiceBoxImage
             src="https://images.unsplash.com/photo-1527515637462-cff94eecc1ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2xlYW5pbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <h4>Flat cleaning</h4>
-          <p>
+          <ServiceBoxTitle>Flat cleaning</ServiceBoxTitle>
+          <ServiceBoxParagraph>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
             accusantium debitis doloribus, repellendus quas ut.
-          </p>
-        </div>
+          </ServiceBoxParagraph>
+        </ServiceBox>
 
-        <div id="Service-box" class="services-5">
-          <img
+        <ServiceBox>
+          <ServiceBoxImage
             src="https://images.unsplash.com/photo-1581578731548-c64695cc6952?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8Y2xlYW5pbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <h4>Flat cleaning</h4>
-          <p>
+          <ServiceBoxTitle>Flat cleaning</ServiceBoxTitle>
+          <ServiceBoxParagraph>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
             accusantium debitis doloribus, repellendus quas ut.
-          </p>
-        </div>
+          </ServiceBoxParagraph>
+        </ServiceBox>
 
-        <button id="button">Book your cleaning</button>
-      </div>
-    </div>
+        <ServButton>Book your cleaning</ServButton>
+      </ServGeral>
+    </Services>
   );
 }
 
--- src/containers/TestimonialsSection/index.js
+++ src/containers/TestimonialsSection/index.js
@@ -3,46 +3,80 @@ import "../../assets/styles/global.css";
 import styled from "styled-components";
 import "../../App.css";
 
+const Testimonials = styled.div`
+  width: 100%;
+  height: auto;
+  padding-top: 180px;
+`;
+
+const TestimonialsH2 = styled.h2`
+  text-align: center;
+  padding: 320px 648px 68px 649px;
+`;
+
+const TestimonalsBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const TestBox = styled.div`
+  width: 360px;
+  height: 236px;
+  border-radius: 5px;
+  background-color: #3ab3b2;
+  text-align: center;
+`;
+
+const TestBoxTitle = styled.h3`
+  padding: 25px 125px 19px 126px;
+`;
+
+const TestBoxParagraph = styled.p`
+  padding: 0px 25px 25px 25px;
+`;
+
+const TestBoxSubTitle = styled.b``;
+
 function TestimonialsSection() {
   return (
-    <div id="testimonials">
-      <h2>TESTIMONIALS</h2>
+    <Testimonials>
+      <TestimonialsH2>TESTIMONIALS</TestimonialsH2>
 
-      <div id="testimonals-box">
-        <div id="test-box-1">
-          <h3>Joao</h3>
-          <p>
-            <b>Toronto - Canada</b>
+      <TestimonalsBox>
+        <TestBox>
+          <TestBoxTitle>Joao</TestBoxTitle>
+          <TestBoxParagraph>
+            <TestBoxSubTitle>Toronto - Canada</TestBoxSubTitle>
             <br />
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Consequuntur quidem, quasi optio eveniet, dolores inventore
             veritatis ex ut ipsum quae aliquid incidunt cumque distinctio? Unde?
-          </p>
-        </div>
+          </TestBoxParagraph>
+        </TestBox>
 
-        <div id="test-box-2">
-          <h3>Joao</h3>
-          <p>
-            <b>Toronto - Canada</b>
+        <TestBox>
+          <TestBoxTitle>Joao</TestBoxTitle>
+          <TestBoxParagraph>
+            <TestBoxSubTitle>Toronto - Canada</TestBoxSubTitle>
             <br />
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Consequuntur quidem, quasi optio eveniet, dolores inventore
             veritatis ex ut ipsum quae aliquid incidunt cumque distinctio? Unde?
-          </p>
-        </div>
+          </TestBoxParagraph>
+        </TestBox>
 
-        <div id="test-box-3">
-          <h3>Joao</h3>
-          <p>
-            <b>Toronto - Canada</b>
+        <TestBox>
+          <TestBoxTitle>Joao</TestBoxTitle>
+          <TestBoxParagraph>
+            <TestBoxSubTitle>Toronto - Canada</TestBoxSubTitle>
             <br />
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Consequuntur quidem, quasi optio eveniet, dolores inventore
             veritatis ex ut ipsum quae aliquid incidunt cumque distinctio? Unde?
-          </p>
-        </div>
-      </div>
-    </div>
+          </TestBoxParagraph>
+        </TestBox>
+      </TestimonalsBox>
+    </Testimonials>
   );
 }
 
