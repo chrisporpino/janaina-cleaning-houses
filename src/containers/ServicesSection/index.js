c8e15507c01cf44d0514c31002613616cbb3ce57
agenciaaudax <agenciaaudax@gmail.com>
2021-01-18 19:38:23 +0000

    atualizacao

--- src/containers/ServicesSection/index.js
+++ src/containers/ServicesSection/index.js
@@ -33,6 +33,7 @@ const ServiceBox = styled.div`
   background-color: rgba(18, 17, 3, 1);
   margin-bottom: 50px;
   border-radius: 10px;
+  flex-shrink: 0;
 `;
 
 const ServiceBoxImage = styled.img`
@@ -77,6 +78,35 @@ const ServButton = styled.button`
   border: none;
 `;
 
+const DropDownButton = styled.button`
+  background-color: rgba(18, 17, 3, 1);
+  color: white;
+  padding: 0px;
+  font-size: 16px;
+  border: none;
+`;
+
+const DropDown = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const DropDowncontent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: blue;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(18, 17, 3, 1)
+  z-index: 1;
+`;
+
+const DropDownLink = styled.a`
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+`;
+
 function ServicesSection() {
   return (
     <Services>
@@ -88,10 +118,20 @@ function ServicesSection() {
             src="https://images.unsplash.com/photo-1577412647305-991150c7d163?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fG9mZmljZXxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <ServiceBoxTitle>Office cleaning</ServiceBoxTitle>
+          <ServiceBoxTitle>House or apartment </ServiceBoxTitle>
           <ServiceBoxParagraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-            accusantium debitis doloribus, repellendus quas ut.
+            With thorough cleaning, your home will also create a safe, clean and
+            comfortable environment for you and your family.
+            <br></br>
+            <br></br>
+            <DropDown>
+              <DropDownButton>It's included </DropDownButton>
+              <DropDowncontent>
+                <DropDownLink>oi</DropDownLink>
+                <DropDownLink>oi</DropDownLink>
+                <DropDownLink>oi</DropDownLink>
+              </DropDowncontent>
+            </DropDown>
           </ServiceBoxParagraph>
         </ServiceBox>
 
@@ -100,10 +140,10 @@ function ServicesSection() {
             src="https://images.unsplash.com/photo-1516455590571-18256e5bb9ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGhvbWV8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <ServiceBoxTitle>Home cleaning</ServiceBoxTitle>
+          <ServiceBoxTitle>Office</ServiceBoxTitle>
           <ServiceBoxParagraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-            accusantium debitis doloribus, repellendus quas ut.
+            We provides a wide variety of general office cleaning and highly
+            customized solutions for your work environment.
           </ServiceBoxParagraph>
         </ServiceBox>
 
@@ -112,10 +152,10 @@ function ServicesSection() {
             src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGhvbWV8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <ServiceBoxTitle>Flat cleaning</ServiceBoxTitle>
+          <ServiceBoxTitle>Deep clean </ServiceBoxTitle>
           <ServiceBoxParagraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-            accusantium debitis doloribus, repellendus quas ut.
+            I perform a deep, quick and efficient cleaning that meets your
+            needs, with a super affordable price.
           </ServiceBoxParagraph>
         </ServiceBox>
 
@@ -124,10 +164,10 @@ function ServicesSection() {
             src="https://images.unsplash.com/photo-1527515637462-cff94eecc1ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2xlYW5pbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <ServiceBoxTitle>Flat cleaning</ServiceBoxTitle>
+          <ServiceBoxTitle>Move In or Out Cleaning</ServiceBoxTitle>
           <ServiceBoxParagraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-            accusantium debitis doloribus, repellendus quas ut.
+            I perform a complete, quick and efficient cleaning that meets your
+            needs.
           </ServiceBoxParagraph>
         </ServiceBox>
 
@@ -136,10 +176,10 @@ function ServicesSection() {
             src="https://images.unsplash.com/photo-1581578731548-c64695cc6952?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8Y2xlYW5pbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt=""
           />
-          <ServiceBoxTitle>Flat cleaning</ServiceBoxTitle>
+          <ServiceBoxTitle>cleaning Weekly, Biweekly, Monthly </ServiceBoxTitle>
           <ServiceBoxParagraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-            accusantium debitis doloribus, repellendus quas ut.
+            Hire our service constantly to ensure the cleanliness, organization
+            and comfort of your home or business.
           </ServiceBoxParagraph>
         </ServiceBox>
 
